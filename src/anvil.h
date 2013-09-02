d68a345277c339b9f4fec4c1e4a82d0add947e09
Fedor Indutny <fedor.indutny@gmail.com>
2013-09-02 15:11:33 +0400

    anvil: fix header

--- src/anvil.h
+++ src/anvil.h
@@ -1,8 +1,8 @@
 #ifndef SRC_ANVIL_H_
 #define SRC_ANVIL_H_
 
-#include "nbt.h"
+#include "common.h"
 
-int mc_anvil_parse(const unsigned char* data, int len, mc_nbt_value_t** out);
+int mc_anvil_parse(const unsigned char* data, int len, mc_region_t** out);
 
 #endif  /* SRC_ANVIL_H_ */
